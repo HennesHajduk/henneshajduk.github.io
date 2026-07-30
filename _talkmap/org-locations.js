4c86cfa7b4e0f3578f514cceee6ad6d5af43be97
Hennes Hajduk <hennes.hajduk@math.tu-dortmund.de>
2026-07-30 19:18:45 +0200

    .

--- _talkmap/org-locations.js
+++ _talkmap/org-locations.js
@@ -1,162 +1,162 @@
 var addressPoints = [
   [
-    "<strong>Modified shallow-water equations for direct bathymetry reconstruction</strong><br>Sep 2017<br>SIAM GS: SIAM Conference on Mathematical and Computational Issues in the Geosciences<br>Erlangen, Germany",
-    49.5891571,
-    10.9812072
-  ],
-  [
-    "<strong>Modified shallow-water equations for direct bathymetry reconstruction</strong><br>Dec 2017<br>Numerical Methods for Shallow Water Equations and Related Models<br>Shenzhen, China",
-    22.5445741,
-    114.0545429
-  ],
-  [
-    "<strong>FESTUNG: An introduction to the software and example application on the basis of the shallow-water equations</strong><br>Dec 2017<br>Mechanics Institute, Chinese Academy of Sciences<br>Beijing, China",
-    39.9057136,
-    116.3912972
-  ],
-  [
-    "<strong>Modified shallow-water equations for direct bathymetry reconstruction</strong><br>Feb 2018<br>Topical Problems of Fluid Mechanics<br>Prague, Czech Republic",
-    50.0874654,
-    14.4212535
-  ],
-  [
-    "<strong>Matrix-free advection-based remap algorithms for Lagrangian/ALE methods</strong><br>Feb 2019<br>SIAM CSE: SIAM Conference on Computational Science and Engineering<br>Spokane, USA",
-    47.6571934,
-    -117.42351
-  ],
-  [
-    "<strong>Matrix-free advection-based remap algorithms for Lagrangian/ALE methods</strong><br>Mar 2019<br>Center for Applied Scientific Computing, LLNL<br>Livermore, California",
-    37.6820583,
-    -121.768053
-  ],
-  [
-    "<strong>Directional vector limiters for discontinuous Galerkin shallow-water models</strong><br>Jul 2019<br>ICIAM: International Congress on Industrial and Applied Mathematics<br>Valencia, Spain",
-    39.4697065,
-    -0.3763353
-  ],
-  [
-    "<strong>Bound-Preserving High-Order Finite Element Schemes for Advection Problems</strong><br>Sep 2019<br>MultiMat: 9th International Conference on Numerical Methods for Multi-Material Fluid Flow<br>Trento, Italy",
-    46.1029536,
-    11.1297425
-  ],
-  [
-    "<strong>Bound-preserving high-order finite element schemes for advection problems</strong><br>Feb 2020<br>Institute of Mathematics, University of Zürich<br>Zürich, Switzerland",
-    47.3744489,
-    8.5410422
-  ],
-  [
-    "<strong>Monolithic convex limiting in high order discontinuous Galerkin discretizations of hyperbolic conservation laws</strong><br>Sep 2020<br>Algoritmy: Conference on Scientific Computing<br>Vysoké Tatry, Slovakia",
-    49.1999166,
-    20.179831
-  ],
-  [
-    "<strong>Monolithic convex limiting in high order discontinuous Galerkin discretizations of hyperbolic conservation laws</strong><br>Oct 2020<br>MoST: Modeling and Simulation of Transport Phenomena (as co-organizer)<br>Treis-Karden, Germany",
-    50.1754283,
-    7.3008675
-  ],
-  [
-    "<strong>Algebraically stabilized enriched Galerkin methods for shallow water flow</strong><br>Jun 2021<br>SIAM GS: SIAM Conference on Mathematical and Computational Issues in the Geosciences<br>Milano, Italy<br><em>(online)</em>",
-    45.4641943,
-    9.1896346
-  ],
-  [
-    "<strong>Property-preserving discontinuous Galerkin methods for hyperbolic conservation laws</strong><br>Aug 2021<br>Banff International Research Station for Mathematical Innovation and Discovery: Bound-Preserving Space and Time Discretizations for Convection-Dominated Problems<br>Casa Matemática Oaxaca, Mexico<br><em>(online)</em>",
-    23.6585116,
-    -102.0077097
-  ],
-  [
-    "<strong>Algebraic flux correction for high-order discontinuous Galerkin methods</strong><br>Sep 2021<br>Hirschegg workshop on conservation laws<br>Hirschegg, Austria",
-    47.342668,
-    10.167689
-  ],
-  [
-    "<strong>Bound-preserving and entropy-stable algebraic flux correction schemes for the shallow water equations with topography</strong><br>Jun 2022<br>ECCOMAS Congress 2022: 8th European Congress on Computational Methods in Applied Sciences and Engineering<br>Oslo, Norway",
-    59.9133301,
-    10.7389701
-  ],
-  [
-    "<strong>Bound-preserving and entropy-stable algebraic flux correction schemes for the shallow water equations with topography</strong><br>Jul 2022<br>ICCFD11: Eleventh International Conference on Computational Fluid Dynamics<br>Maui, USA",
+    "Bound-preserving and entropy-stable algebraic flux correction schemes for the shallow water equations with topography<br />Jul 2022, Maui \u2014 ICCFD11: Eleventh International Conference on Computational Fluid Dynamics",
     20.8029568,
     -156.3106833
   ],
   [
-    "<strong>Property-preserving discontinuous Galerkin methods for hyperbolic problems</strong><br>Jul 2022<br>WCCM-APCOM: 15th World Congress on Computational Mechanics & 8th Asian Pacific Congress on Computational Mechanics<br>Yokohama, Japan<br><em>(online)</em>",
-    35.4503381,
-    139.6343802
-  ],
-  [
-    "<strong>On the need to enforce discrete entropy inequalities in numerical methods for hyperbolic problems</strong><br>Sep 2022<br>MultiMat: 10th International Conference on Numerical Methods for Multi-Material Fluid Flow<br>Zürich, Switzerland",
-    47.3744489,
-    8.5410422
-  ],
-  [
-    "<strong>Bound-preserving, entropy-stable, and well-balanced algebraic flux correction schemes for the shallow water equations with topography</strong><br>Oct 2022<br>Essentially hyperbolic problems: unconventional numerics, and applications<br>Monte Verità, Switzerland",
-    46.1582933,
-    8.7630297
-  ],
-  [
-    "<strong>Property-preserving finite element methods for hyperbolic problems</strong><br>Dec 2023<br>Mechanics Research Group, University of Oslo<br>Oslo, Norway",
-    59.9133301,
-    10.7389701
-  ],
-  [
-    "<strong>The effects of bottom topography on the spin-up of stratified ocean models</strong><br>Dec 2023<br>Chair of Applied Mathematics and Numerics, TU Dortmund University<br>Dortmund, Germany",
-    51.5142273,
-    7.4652789
-  ],
-  [
-    "<strong>Monolithic convex limiting techniques for finite element methods applied to hyperbolic PDEs</strong><br>Feb 2024<br>School of Engineering Science, Lappeenranta-Lahti University of Technology<br>Lappeenranta, Finland",
-    61.0583417,
-    28.1862293
-  ],
-  [
-    "<strong>Recent advances in algebraic flux-correction (AFC) schemes for discontinuous Galerkin discretizations</strong><br>May 2024<br>Trends in Scientific Computing -- Celebrating 30 years of Scientific Computing in Dortmund<br>Dortmund, Germany",
-    51.5142273,
-    7.4652789
-  ],
-  [
-    "<strong>Recent advances in algebraic flux-correction (AFC) schemes for discontinuous Galerkin discretizations</strong><br>May 2024<br>Trends in Scientific Computing<br>Dortmund, Germany",
-    51.5142273,
-    7.4652789
-  ],
-  [
-    "<strong>Spin-up of a stratified ocean with topography</strong><br>Jun 2025<br>ICGAFD: International Conference on Geophysical and Astrophysical Fluid Dynamics<br>Plouzané, France",
-    48.3817165,
-    -4.6212044
-  ],
-  [
-    "<strong>Failsafe limiting for high-order WENO-stabilized continuous Galerkin discretizations of hyperbolic systems</strong><br>Jul 2025<br>MoST: Modeling and Simulation of Transport Phenomena<br>Treis-Karden, Germany",
-    50.1754283,
-    7.3008675
-  ],
-  [
-    "<strong>Shallow water modeling and property-preserving numerics</strong><br>Feb 2026<br>Mathematics of thin materials structures<br>Dresden, Germany",
-    51.0493286,
-    13.7381437
-  ],
-  [
-    "<strong>Failsafe continuous Galerkin discretizations with stabilizations based on artificial intelligence</strong><br>May 2026<br>HYP2026: Hyperbolic Problems: Theory, Numerics and Applications<br>Stuttgart, Germany",
-    48.7784485,
-    9.1800132
-  ],
-  [
-    "<strong>Well-balanced, entropy-stable, and positivity-preserving schemes for shallow water models</strong><br>Jun 2026<br>Workshop Mathematics and Environment<br>Hanover, Germany",
+    "Well-balanced, entropy-stable, and positivity-preserving schemes for shallow water models<br />Jun 2026, Hanover \u2014 Workshop Mathematics and Environment",
     52.3744779,
     9.7385532
   ],
   [
-    "<strong>Geophysical fluid dynamics: Model derivation and property-preserving numerical methods</strong><br>Jul 2026<br>Institute of Mathematics, TU Clausthal<br>Clausthal-Zellerfeld, Germany",
+    "Failsafe limiting for high-order WENO-stabilized continuous Galerkin discretizations of hyperbolic systems<br />Jul 2025, Treis-Karden \u2014 MoST: Modeling and Simulation of Transport Phenomena",
+    50.1754283,
+    7.3008675
+  ],
+  [
+    "Geophysical fluid dynamics: Model derivation and property-preserving numerical methods<br />Jul 2026, Clausthal-Zellerfeld \u2014 Institute of Mathematics, TU Clausthal",
     51.8070405,
     10.3364483
   ],
   [
-    "<strong>A property-preserving second-order scheme for steady and unsteady compressible Navier–Stokes equations</strong><br>Jul 2026<br>WCCM-ECCOMAS: 17th World Congress on Computational Mechanics & 10th European Congress on Computational Methods in Applied Sciences and Engineering<br>Munich, Germany",
+    "Recent advances in failsafe high-resolution schemes for hyperbolic problems<br />Dec 2026, Saarbr\u00fccken \u2014 Saarbr\u00fccken University",
+    49.234362,
+    6.996379
+  ],
+  [
+    "Algebraic flux correction for high-order discontinuous Galerkin methods<br />Sep 2021, Hirschegg \u2014 Hirschegg workshop on conservation laws",
+    47.342668,
+    10.167689
+  ],
+  [
+    "Modified shallow-water equations for direct bathymetry reconstruction<br />Feb 2018, Prague \u2014 Topical Problems of Fluid Mechanics",
+    50.0874654,
+    14.4212535
+  ],
+  [
+    "A property-preserving second-order scheme for steady and unsteady compressible Navier\u2013Stokes equations<br />Jul 2026, Munich \u2014 WCCM-ECCOMAS: 17th World Congress on Computational Mechanics & 10th European Congress on Computational Methods in Applied Sciences and Engineering",
     48.1371079,
     11.5753822
   ],
   [
-    "<strong>Recent advances in failsafe high-resolution schemes for hyperbolic problems</strong><br>Dec 2026<br>Saarbrücken University<br>Saarbrücken, Germany",
-    49.234362,
-    6.996379
+    "Monolithic convex limiting in high order discontinuous Galerkin discretizations of hyperbolic conservation laws<br />Sep 2020, Vysok\u00e9 Tatry \u2014 Algoritmy: Conference on Scientific Computing",
+    49.1999166,
+    20.179831
+  ],
+  [
+    "Property-preserving finite element methods for hyperbolic problems<br />Dec 2023, Oslo \u2014 Mechanics Research Group, University of Oslo",
+    59.9133301,
+    10.7389701
+  ],
+  [
+    "On the need to enforce discrete entropy inequalities in numerical methods for hyperbolic problems<br />Sep 2022, Z\u00fcrich \u2014 MultiMat: 10th International Conference on Numerical Methods for Multi-Material Fluid Flow",
+    47.3744489,
+    8.5410422
+  ],
+  [
+    "Property-preserving discontinuous Galerkin methods for hyperbolic conservation laws<br />Aug 2021, Casa Matem\u00e1tica Oaxaca \u2014 Banff International Research Station for Mathematical Innovation and Discovery: Bound-Preserving Space and Time Discretizations for Convection-Dominated Problems",
+    23.6585116,
+    -102.0077097
+  ],
+  [
+    "Modified shallow-water equations for direct bathymetry reconstruction<br />Sep 2017, Erlangen \u2014 SIAM GS: SIAM Conference on Mathematical and Computational Issues in the Geosciences",
+    49.5891571,
+    10.9812072
+  ],
+  [
+    "Bound-preserving, entropy-stable, and well-balanced algebraic flux correction schemes for the shallow water equations with topography<br />Oct 2022, Monte Verit\u00e0 \u2014 Essentially hyperbolic problems: unconventional numerics, and applications",
+    46.1582933,
+    8.7630297
+  ],
+  [
+    "Bound-Preserving High-Order Finite Element Schemes for Advection Problems<br />Sep 2019, Trento \u2014 MultiMat: 9th International Conference on Numerical Methods for Multi-Material Fluid Flow",
+    46.1029536,
+    11.1297425
+  ],
+  [
+    "Monolithic convex limiting techniques for finite element methods applied to hyperbolic PDEs<br />Feb 2024, Lappeenranta \u2014 School of Engineering Science, Lappeenranta-Lahti University of Technology",
+    61.0583417,
+    28.1862293
+  ],
+  [
+    "Recent advances in algebraic flux-correction (AFC) schemes for discontinuous Galerkin discretizations<br />May 2024, Dortmund \u2014 Trends in Scientific Computing",
+    51.5142273,
+    7.4652789
+  ],
+  [
+    "Algebraically stabilized enriched Galerkin methods for shallow water flow<br />Jun 2021, Milano \u2014 SIAM GS: SIAM Conference on Mathematical and Computational Issues in the Geosciences",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "Matrix-free advection-based remap algorithms for Lagrangian/ALE methods<br />Feb 2019, Spokane \u2014 SIAM CSE: SIAM Conference on Computational Science and Engineering",
+    47.6571934,
+    -117.42351
+  ],
+  [
+    "Spin-up of a stratified ocean with topography<br />Jun 2025, Plouzan\u00e9 \u2014 ICGAFD: International Conference on Geophysical and Astrophysical Fluid Dynamics",
+    48.3817165,
+    -4.6212044
+  ],
+  [
+    "Matrix-free advection-based remap algorithms for Lagrangian/ALE methods<br />Mar 2019, Livermore \u2014 Center for Applied Scientific Computing, LLNL",
+    37.6820583,
+    -121.768053
+  ],
+  [
+    "Recent advances in algebraic flux-correction (AFC) schemes for discontinuous Galerkin discretizations<br />May 2024, Dortmund \u2014 Trends in Scientific Computing -- Celebrating 30 years of Scientific Computing in Dortmund",
+    51.5142273,
+    7.4652789
+  ],
+  [
+    "Modified shallow-water equations for direct bathymetry reconstruction<br />Dec 2017, Shenzhen \u2014 Numerical Methods for Shallow Water Equations and Related Models",
+    22.5445741,
+    114.0545429
+  ],
+  [
+    "Bound-preserving and entropy-stable algebraic flux correction schemes for the shallow water equations with topography<br />Jun 2022, Oslo \u2014 ECCOMAS Congress 2022: 8th European Congress on Computational Methods in Applied Sciences and Engineering",
+    59.9133301,
+    10.7389701
+  ],
+  [
+    "Monolithic convex limiting in high order discontinuous Galerkin discretizations of hyperbolic conservation laws<br />Oct 2020, Treis-Karden \u2014 MoST: Modeling and Simulation of Transport Phenomena (as co-organizer)",
+    50.1754283,
+    7.3008675
+  ],
+  [
+    "FESTUNG: An introduction to the software and example application on the basis of the shallow-water equations<br />Dec 2017, Beijing \u2014 Mechanics Institute, Chinese Academy of Sciences",
+    39.9057136,
+    116.3912972
+  ],
+  [
+    "Property-preserving discontinuous Galerkin methods for hyperbolic problems<br />Jul 2022, Yokohama \u2014 WCCM-APCOM: 15th World Congress on Computational Mechanics & 8th Asian Pacific Congress on Computational Mechanics",
+    35.4503381,
+    139.6343802
+  ],
+  [
+    "Directional vector limiters for discontinuous Galerkin shallow-water models<br />Jul 2019, Valencia \u2014 ICIAM: International Congress on Industrial and Applied Mathematics",
+    39.4697065,
+    -0.3763353
+  ],
+  [
+    "The effects of bottom topography on the spin-up of stratified ocean models<br />Dec 2023, Dortmund \u2014 Chair of Applied Mathematics and Numerics, TU Dortmund University",
+    51.5142273,
+    7.4652789
+  ],
+  [
+    "Failsafe continuous Galerkin discretizations with stabilizations based on artificial intelligence<br />May 2026, Stuttgart \u2014 HYP2026: Hyperbolic Problems: Theory, Numerics and Applications",
+    48.7784485,
+    9.1800132
+  ],
+  [
+    "Shallow water modeling and property-preserving numerics<br />Feb 2026, Dresden \u2014 Mathematics of thin materials structures",
+    51.0493286,
+    13.7381437
+  ],
+  [
+    "Bound-preserving high-order finite element schemes for advection problems<br />Feb 2020, Z\u00fcrich \u2014 Institute of Mathematics, University of Z\u00fcrich",
+    47.3744489,
+    8.5410422
   ]
 ];
